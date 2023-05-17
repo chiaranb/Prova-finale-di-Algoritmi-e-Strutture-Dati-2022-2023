d60370a615d66be6ad97b36663043eb1b09ebed5
ChiaraNB <chiarathien.nguyen@mail.polimi.it>
2023-05-17 14:58:38 +0200

    First commit.

---
+++ main.c
@@ -0,0 +1,12 @@
+//
+// Created by Chiara Nguyen on 17/05/23.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    printf("Welcome to the game of Hangman!\n");
+}
